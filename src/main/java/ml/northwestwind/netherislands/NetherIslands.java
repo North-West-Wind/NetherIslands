1524932669d998cb39b216665d247e2f241a1782
North-West-Wind <wsyn148@gmail.com>
2022-08-20 15:15:20 +0800

    first push

---
+++ src/main/java/ml/northwestwind/netherislands/NetherIslands.java
@@ -0,0 +1,11 @@
+package ml.northwestwind.netherislands;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(NetherIslands.MOD_ID)
+public class NetherIslands {
+    public static final String MOD_ID = "netherislands";
+
+    public NetherIslands() {
+    }
+}
